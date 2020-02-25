f6035c22d19d4fd3164857e52b6c7260fd5c04b0
aastroza <aastroza@users.noreply.github.com>
2020-02-25 15:37:04 +0000

    deploy: 796594c82f4363c1cfc87fa5f4d427ac3ae72da1

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "https://aastroza.github.io/about/",
     "title": "About Me",
-    "body": "This is where you put the contents of your About page. Like all your pages, it’s in Markdown format. This website is powered by fastpages 1.       a blogging platform that natively supports Jupyter notebooks in addition to other formats.  &#8617;    "
+    "body": "I’m a Machine Learning Engineer / Data Scientist living in Santiago, Chile. You can see my resume and projects here format. This website is powered by fastpages 1.       a blogging platform that natively supports Jupyter notebooks in addition to other formats.  &#8617;    "
     }, {
     "id": 2,
     "url": "https://aastroza.github.io/categories/",
